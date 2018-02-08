74459f0f518528b9297e5675402d773ebb9536ae
Rock King <rockking.jy@gmail.com>
2018-02-08 10:17:15 +0900

    Add files via upload

--- dronekit_getGPS.py
+++ dronekit_getGPS.py
@@ -1,8 +1,32 @@
+import time
+import os.path
 from dronekit import Vehicle,connect
+
+#time.sleep(20)
+
+directory = "/home/nvidia/GPS/"
+ori = "/home/nvidia/GPS/gps.txt"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+i = 0
+ori = directory + "gps" + str(i) + ".txt"
+while os.path.isfile(ori):
+    i = i + 1
+    ori = directory + "gps" + str(i) + ".txt"
+
+f = open(ori, 'a')
+print(ori+" created.")
+
 vehicle = connect('/dev/ttyACM0', wait_ready=True, baud=57600)
-lat = vehicle.location.global_frame.lat
-lon = vehicle.location.global_frame.lon
-heading = vehicle.heading
-print(lat)
-print(lon)
-print(heading)
+
+while(1):
+    current_millitime = int(round(time.time() * 1000))
+    lat = vehicle.location.global_frame.lat
+    lon = vehicle.location.global_frame.lon
+    heading = vehicle.heading
+    print(current_millitime,lat,lon,heading)
+    f.write(str(current_millitime)+':'+str(lat)+','+str(lon)+','+str(heading)+'\n')
+    f.flush()
+    time.sleep(0.5)
+
